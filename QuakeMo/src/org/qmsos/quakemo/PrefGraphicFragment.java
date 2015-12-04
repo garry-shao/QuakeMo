ea9d67d9d4b53443c54c8b1e52070af015b789bb
SS <SS@SSS>
2015-12-04 20:57:53 +0800

    Update string resources.

--- QuakeMo/src/org/qmsos/quakemo/PrefGraphicFragment.java
+++ QuakeMo/src/org/qmsos/quakemo/PrefGraphicFragment.java
@@ -8,13 +8,13 @@ import android.preference.PreferenceFragment;
  * The fragment show the UI part of preferences.
  *
  */
-public class PrefUIFragment extends PreferenceFragment {
+public class PrefGraphicFragment extends PreferenceFragment {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		addPreferencesFromResource(R.xml.preference_ui);
+		addPreferencesFromResource(R.xml.preference_graphic);
 	}
 
 }
