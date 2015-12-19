70152dbd3815698a54eb454e1520f4f5d0072237
SS <SS@SSS>
2015-12-19 16:21:44 +0800

    Remove SHOWMAP feature from detail dialog, can be done by search.

--- QuakeMo/src/org/qmsos/quakemo/fragment/DetailsDialogFragment.java
+++ QuakeMo/src/org/qmsos/quakemo/fragment/DetailsDialogFragment.java
@@ -11,8 +11,6 @@ import org.qmsos.quakemo.R;
 import android.app.Dialog;
 import android.content.ContentUris;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
@@ -26,23 +24,9 @@ import android.support.v7.app.AlertDialog;
 public class DetailsDialogFragment extends DialogFragment {
 
 	/**
-	 * Follow keys are things need to be saved on configuration change.
+	 * Key used to retrieve earthquake id from bundle.
 	 */
 	private static final String KEY_EARTHQUAKE = "KEY_EARTHQUAKE";
-	private static final String KEY_ADDMAP = "KEY_ADDMAP";
-
-	/**
-	 * Flag on whether map button is shown on dialog.
-	 */
-	private boolean mapEnabled;
-
-	/**
-	 * Used by this dialog to communicate with the containing activity.
-	 *
-	 */
-	public interface ShowMapListener {
-		void onShowMap(long id);
-	}
 
 	/**
 	 * Create a new instance of dialog that shows particular earthquake details.
@@ -63,29 +47,12 @@ public class DetailsDialogFragment extends DialogFragment {
 		return fragment;
 	}
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-
-		if (savedInstanceState != null) {
-			mapEnabled = savedInstanceState.getBoolean(KEY_ADDMAP);
-		}
-	}
-
-	@Override
-	public void onSaveInstanceState(Bundle arg0) {
-		arg0.putBoolean(KEY_ADDMAP, mapEnabled);
-
-		super.onSaveInstanceState(arg0);
-	}
 
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
-		final long id = getArguments().getLong(KEY_EARTHQUAKE);
-
 		String dialogDetails;
 
-		DateFormat dataFormat = new SimpleDateFormat("MM/dd/yyyy - HH:mm:ss", Locale.US);
+		final long id = getArguments().getLong(KEY_EARTHQUAKE);
 
 		Cursor cursor = getContext().getContentResolver()
 				.query(ContentUris.withAppendedId(QuakeProvider.CONTENT_URI, id), null, null, null, null);
@@ -97,6 +64,7 @@ public class DetailsDialogFragment extends DialogFragment {
 				String details = cursor.getString(cursor.getColumnIndex(QuakeProvider.KEY_DETAILS));
 				String link = cursor.getString(cursor.getColumnIndex(QuakeProvider.KEY_LINK));
 
+				DateFormat dataFormat = new SimpleDateFormat("MM/dd/yyyy - HH:mm:ss", Locale.US);
 				dialogDetails = dataFormat.format(new Date(time)) + 
 						"\n\n" + "Magnitude: " + magnitude + 
 						"\n\n" + "Depth: " + depth + " km" + 
@@ -110,25 +78,8 @@ public class DetailsDialogFragment extends DialogFragment {
 
 		AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
 		builder.setTitle(R.string.dialog_details_title).setMessage(dialogDetails);
-		if (mapEnabled) {
-			builder.setPositiveButton(R.string.dialog_details_positive, new OnClickListener() {
-
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					((ShowMapListener) getActivity()).onShowMap(id);
-				}
-			});
-		}
 
 		return builder.create();
-	}
-
-	public boolean isMapEnabled() {
-		return mapEnabled;
-	}
-
-	public void setMapEnabled(boolean mapEnabled) {
-		this.mapEnabled = mapEnabled;
 	}
 
 }
